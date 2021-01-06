b13de7a33701e9718d686a4819b17e224ceba857
张仁 <zhang.ren@darcytech.com>
2021-01-06 23:07:20 +0800

    v-1.0.0 amqp

--- mq/src/main/java/com/wxsl/helena/mq/rabbit/RabbitConfiguration.java
+++ mq/src/main/java/com/wxsl/helena/mq/rabbit/RabbitConfiguration.java
@@ -4,10 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.api.RabbitListenerErrorHandler;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,12 +25,7 @@ public class RabbitConfiguration {
     public static final String TOPIC_EXCHANGE = "helena.topic";
 
     @Bean
-    public AmqpAdmin amqpAdmin(ConnectionFactory connectionFactory) {
-        return new RabbitAdmin(connectionFactory);
-    }
-
-    @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, RabbitProperties properties) {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
         return rabbitTemplate;
@@ -66,7 +62,15 @@ public class RabbitConfiguration {
         return BindingBuilder.bind(queue).to(exchange).with(TOPIC_ROUTING_KEY);
     }
 
-    @RabbitListener(queues = DIRECT_QUEUE)
+    @Bean
+    public RabbitListenerErrorHandler directRabbitListenerErrorHandler() {
+        return (amqpMessage, message, exception) -> {
+            log.info("amqpMessage:{},message:{},exception:{}", amqpMessage, message, exception);
+            return exception;
+        };
+    }
+
+    @RabbitListener(queues = DIRECT_QUEUE, errorHandler = "directRabbitListenerErrorHandler")
     public void processDirectQueue(String message) {
         log.info("process direct queue message:{}", message);
     }
